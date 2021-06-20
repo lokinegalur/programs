2d98fd72cb86eb311d60f616b138f4ef3869c6d1
lokinegalur <47582005+lokinegalur@users.noreply.github.com>
2021-06-20 17:57:42 +0530

    Update LCSTAB.java

--- LCSTAB.java
+++ LCSTAB.java
@@ -7,7 +7,7 @@ public class LCSTAB{
         for(int i=1;i<=m;i++){
             for(int j=1;j<=n;j++){
                 if(s1.charAt(i-1)==s2.charAt(j-1)){
-                    dp[i][j]=1+dp[i][j-1];
+                    dp[i][j]=1+dp[i-1][j-1];
                 }
                 else{
                     dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
