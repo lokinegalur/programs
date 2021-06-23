6e52d12edb2e07252a82c4829af7e62d0751f841
lokinegalur <47582005+lokinegalur@users.noreply.github.com>
2021-06-23 11:26:41 +0530

    Balanced parenthesis

---
+++ Balanced_Parenthesis.java
@@ -0,0 +1,57 @@
+class Balanced_Parenthesis
+{
+
+    //Function to check if brackets are balanced or not.
+    
+static boolean ispar(String str)
+    {
+
+        Stack<Character> s = new Stack<>();
+
+        for(int i=0;i<str.length();i++){
+
+            char ch = str.charAt(i);
+
+            if(ch=='[' || ch=='{' || ch=='(')
+
+                s.push(ch);
+
+            else{
+
+                if(s.isEmpty())
+
+                    return false;
+
+                char top = s.peek();
+
+                if(top=='[' && ch==']')
+
+                    s.pop();
+
+                else if(top=='{' && ch=='}')
+
+                    s.pop();
+
+                else if(top=='(' && ch==')')
+
+                    s.pop();
+
+                else
+ 
+                    return false;
+
+            }
+
+        }
+
+        if(s.isEmpty())
+
+            return true;
+
+        else
+
+            return false;
+
+    }
+
+}
