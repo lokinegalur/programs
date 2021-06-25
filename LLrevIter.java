1842544695dd26a8e0e834c04640d2ebd5d5d31b
lokinegalur <47582005+lokinegalur@users.noreply.github.com>
2021-06-25 09:55:22 +0530

    LL reverse Iterative

---
+++ LLrevIter.java
@@ -0,0 +1,24 @@
+class LLrevIter
+{
+
+     Node reverseList(Node head)
+    {
+
+        Node prev=null,curr=head,next=null;
+
+        while(curr!=null){
+
+            next=curr.next;
+
+            curr.next=prev;
+
+            prev=curr;
+
+            curr=next;
+
+        }
+        return prev;
+
+    }
+
+}
