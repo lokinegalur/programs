f6b393a1b85a1b64ca3e31f12dca811c173acaf1
lokinegalur <47582005+lokinegalur@users.noreply.github.com>
2021-06-27 21:56:29 +0530

    Rotate a matrix by 90 degrees

---
+++ MatrixRotate.java
@@ -0,0 +1,50 @@
+class MatrixRotate {
+
+    static void swap(int i,int j,int[][] mat){
+
+        int temp = mat[i][j];
+ 
+        mat[i][j]=mat[j][i];
+
+        mat[j][i]=temp;
+
+    }
+     static void rowSwap(int i,int j,int k,int[][] mat){
+
+        int temp = mat[i][j];
+
+        mat[i][j]=mat[i][k];
+
+        mat[i][k]=temp;
+
+    }
+
+    public void rotate(int[][] matrix) {
+
+        int n=matrix.length;
+
+        for(int i=0;i<n;i++){
+
+            for(int j=i;j<n;j++){
+
+                swap(i,j,matrix);
+
+            }
+
+        }
+
+
+        
+   for(int i=0;i<n;i++){
+
+            for(int j=0;j<n/2;j++){
+
+                rowSwap(i,j,n-j-1,matrix);
+
+            }
+
+        }
+
+     }
+
+}
