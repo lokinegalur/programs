a45a54907d6b02da7f951ae9cc44558dc9d3758b
lokinegalur <47582005+lokinegalur@users.noreply.github.com>
2021-06-27 21:02:50 +0530

    Matrix rotate by 90 degrees

--- Solution.java
+++ Solution.java
@@ -1,50 +1,28 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    public List<List<Integer>> levelOrderBottom(TreeNode root) {
-        List<List<Integer>> mainList = new ArrayList<List<Integer>>();  //list of list
-        if(root==null)
-            return mainList;
-         
-        Queue<TreeNode> q = new LinkedList<>();
-        q.offer(root);
-        int looper = 1;
-        List<Integer> xx = new ArrayList<>();
-        xx.add(root.val);
-        mainList.add(xx);
-        while(!q.isEmpty()){
-            List<Integer> l = new ArrayList<>();   // list for current level
-            int i=0,temp=0;
-            while(i++<looper){
-                TreeNode x = q.poll();
-                if(x.left!=null){
-                    q.offer(x.left);
-                    temp++;
-                    l.add(x.left.val);
-                }
-                if(x.right!=null){
-                    q.offer(x.right);
-                    temp++;
-                    l.add(x.right.val);
-                }
+    static void swap(int i,int j,int[][] mat){
+        int temp = mat[i][j];
+        mat[i][j]=mat[j][i];
+        mat[j][i]=temp;
+    }
+    
+    static void rowSwap(int i,int j,int k,int[][] mat){
+        int temp = mat[i][j];
+        mat[i][j]=mat[i][k];
+        mat[i][k]=temp;
+    }
+    public void rotate(int[][] matrix) {
+        int n=matrix.length;
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                swap(i,j,matrix);
             }
-            if(temp>0)
-              mainList.add(0,l);
-            looper=temp;
         }
-        return mainList;
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n/2;j++){
+                rowSwap(i,j,n-j-1,matrix);
+            }
+        }
+        
     }
 }
